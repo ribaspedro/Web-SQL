f9af65a8edb7608bcd8119ec76122ec1eb253572
Pedro Ribas <pedro.ribas@fatec.sp.gov.br>
2017-10-23 17:04:47 -0200

    Adding and setting up npm dependencies

---
+++ public/javascripts/web-sql.js
@@ -0,0 +1,13 @@
+
+$(document).ready(function() {
+
+    var map = new ol.Map({
+        target: 'map',
+        view: new ol.View({
+        extent: [-180, -90, 180, 90],
+        center: [-55, -15],
+        zoom: 4,
+        projection: ol.proj.get("EPSG:4326")
+        })
+    });
+});
